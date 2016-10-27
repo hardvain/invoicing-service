2456fe616d675d1bb664774d8fc9f1bea699967e
Aravindh Sridharan <mail@aravindh.io>
2016-10-27 08:37:55 +0530

    Added test for date time parsing

---
+++ src/test/scala/com/acme/invoiceservice/models/InvoiceProtocolSpec.scala
@@ -0,0 +1,28 @@
+package com.acme.invoiceservice.models
+
+import org.scalatest.{FreeSpec, Matchers}
+import InvoiceProtocol._
+import org.joda.time.DateTime
+import spray.json._
+class InvoiceProtocolSpec extends FreeSpec with Matchers{
+  "InvoiceProtocol"-{
+    "should convert json to DateTime correctly" in {
+      val time: DateTime = "2016-10-27T08:22:15.919+05:30".parseJson.convertTo[DateTime]
+      assert(time != null)
+    }
+
+    "should throw error when given wrong json for date" in {
+      val exception = intercept[DeserializationException]{
+        """{"date":"2016"}""".parseJson.convertTo[DateTime]
+      }
+
+      assert(exception.getMessage == """{"date":"2016"} cannot be converted to date time""")
+    }
+
+    "should convert date time to string properly" in {
+      val dateTime: DateTime = DateTime.now()
+      val json: JsValue = dateTime.toJson
+      assert(json == JsString(dateTime.toString()))
+    }
+  }
+}
